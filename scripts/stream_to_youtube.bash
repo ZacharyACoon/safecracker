0417371de884f09aa0410de10e68b88ecbbb037a
ZacharyACoon <ZacharyACoon@users.noreply.github.com>
2022-06-12 04:54:05 +0100

    fix keyframe rate.

--- scripts/stream_to_youtube.bash
+++ scripts/stream_to_youtube.bash
@@ -6,7 +6,7 @@ ffmpeg \
   -f lavfi -i anullsrc \
   -f v4l2 -framerate 11 -video_size 640x480 \
   -i /dev/video0 \
-  -c:v libx264 -preset ultrafast -crf 32 -r 30 -g 60 -keyint_min 30 \
+  -c:v libx264 -preset ultrafast -crf 32 -r 30 -g 30 -keyint_min 30 \
   -c:a aac \
   -f flv -vf "format=yuv420p" \
   rtmp://a.rtmp.youtube.com/live2/${1}
